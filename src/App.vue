2463e027cca4147403d9760a16f60ee2b09e12e7
권준학 <junhakkwon@gwonjunhag-ui-MacBookPro.local>
2021-04-12 23:10:04 +0900

    Woring walkpage

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div>
     <router-view></router-view>
   </div>
 </template>
@@ -15,10 +15,49 @@ export default {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  margin: 0 4%;
 }
-
 .primaryColor {
   color: #0c039e;
+}
+.font-bold {
+  font-weight: bold;
+}
+.text-center {
+  text-align: center;
+}
+.text-right {
+  text-align: right;
+}
+.mt-5 {
+  margin-top: 5px;
+}
+.ml-5 {
+  margin-left: 5px;
+}
+.mr-5 {
+  margin-right: 5px;
+}
+.my-5 {
+  margin-top: 5px;
+  margin-bottom: 5px;
+}
+.my-10 {
+  margin-top: 10px;
+  margin-bottom: 10px;
+}
+.p-5 {
+  padding: 5px;
+}
+.pl-5 {
+  padding-left: 5px;
+}
+.pr-5 {
+  padding-right: 5px;
+}
+.pl-10 {
+  padding-left: 10px;
+}
+.pr-10 {
+  padding-right: 10px;
 }
 </style>
--- src/page/Walk.vue
+++ src/page/Walk.vue
@@ -1,10 +1,97 @@
 <template>
-  <div>Walk</div>
+  <div>
+    <img src="@/assets/walkBanner.jpeg" style="width: 100%" />
+    <el-container>
+      <el-main>
+        <el-row class="infoWalk p-5">
+          <el-col :span="12" class="pl-10">000 님</el-col>
+          <el-col :span="12" class="text-right pr-10">000 걸음</el-col>
+        </el-row>
+
+        <div class="my-10">
+          <div class="titleText">Today 브리핑</div>
+          <el-row :gutter="5" class="my-5">
+            <el-col :span="12">
+              <div class="briefingWrap walkBriefing">
+                <div class="font-bold">걷기운동</div>
+                <div class="font-bold">4시간 30분</div>
+                <div>맞춤권장: 1시간 10분</div>
+              </div>
+            </el-col>
+            <el-col :span="12">
+              <div class="briefingWrap powerWalkingBriefing">
+                <div class="font-bold">파워워킹</div>
+                <div class="font-bold">30분</div>
+                <div>맞춤권장: 20분</div>
+              </div>
+            </el-col>
+          </el-row>
+          <el-row :gutter="5" class="my-5">
+            <el-col :span="12">
+              <div class="briefingWrap sleepBriefing">
+                <div class="font-bold">수면</div>
+                <div class="font-bold">4시간 30분</div>
+                <div>맞춤권장: 7시간</div>
+              </div> </el-col
+            ><el-col :span="12">
+              <div class="briefingWrap burnKcal">
+                <div class="font-bold">걷기운동</div>
+                <div class="font-bold">4시간 30분</div>
+                <div>맞춤권장: 1시간 10분</div>
+              </div>
+            </el-col>
+          </el-row>
+        </div>
+
+        <hr />
+
+        <div>
+          <div class="titleText">2021년 4월 15일 목요일</div>
+          <div class="mt-5">금일 건강관리 점수 : 종합 B+</div>
+          <div class="explainText">
+            전체평균: B+[상위 10%], 내가 가입한 그룹 평균 B-[상위 6%]
+          </div>
+        </div>
+      </el-main>
+    </el-container>
+  </div>
 </template>
 
 <script>
-export default {};
+export default {
+  name: "Walk",
+};
 </script>
 
 <style>
+.titleText {
+  color: #666666;
+}
+.explainText {
+  color: #383eae;
+  font-size: 13px;
+}
+.infoWalk {
+  background-color: #353bad;
+  color: white;
+  border-radius: 5px;
+}
+.briefingWrap {
+  padding: 10px;
+  border-radius: 5px;
+  color: white;
+  font-size: 14px;
+}
+.walkBriefing {
+  background-color: #8bcde7;
+}
+.powerWalkingBriefing {
+  background-color: #75a8e7;
+}
+.sleepBriefing {
+  background-color: #4689cc;
+}
+.burnKcal {
+  background-color: #cc56ff;
+}
 </style>
